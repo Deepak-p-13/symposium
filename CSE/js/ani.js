01c6b3d2d3938f5fa8fbb80d40cc84d69bd7cc2c
Akhilesh <akhilesh.2101005@srec.ac.in>
2024-09-18 23:35:47 +0530

    New

---
+++ CSE/js/ani.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const sr = ScrollReveal({
+        origin: 'top',
+        distance: '60px',
+        duration: 2000,
+        delay: 200,
+       
+         //reset: true 
+    });
+    sr.reveal('.countdown-container #countdown div', { interval: 200, delay: 150 });
+    sr.reveal('#but', { delay: 300 });
+    sr.reveal('.container #abouts div', { interval: 200,delay: 400 });
+    sr.reveal('.highlighted-cards', { delay: 500 });
+    sr.reveal('#tech .row div', { interval: 200, delay: 150 });
+    sr.reveal('#nontech .row div', { interval: 200, delay: 150 });
+    sr.reveal('#photo .item a', { interval: 200, delay: 150 });
+    sr.reveal('#blog-section .row div', { interval: 200, delay: 150 });
+    sr.reveal('.footer .container div', { interval: 200, delay: 150 });
+   //sr.reveal('.floating-icons ', { delay: 150 });
+  // sr.reveal('.floating-video ', { delay: 150 });
+
+
+    
+});
+
+
