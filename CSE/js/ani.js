a7d68c5a91f9144df1f6e0061b3da9f40bb92734
Akhilesh <akhilesh.2101005@srec.ac.in>
2024-09-19 23:22:27 +0530

    Discription

--- CSE/js/ani.js
+++ CSE/js/ani.js
@@ -10,6 +10,8 @@ document.addEventListener('DOMContentLoaded', function () {
     sr.reveal('.container .row h1', { interval: 200, delay: 150 });
     sr.reveal('.countdown-container #countdown div', { interval: 200, delay: 150 });
     sr.reveal('#but', { delay: 300 });
+    sr.reveal('#rules', { delay: 300 });
+    sr.reveal('#rulescon', { delay: 300 });
     sr.reveal('.container #abouts div', { interval: 200,delay: 400 });
     sr.reveal('.highlighted-cards', { delay: 500 });
     sr.reveal('#tech .row div', { interval: 200, delay: 150 });
